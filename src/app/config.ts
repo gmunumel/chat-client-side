c4dcb2aaff2420c6911b5ba48f3630f7f6afc4d2
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-06-10 11:51:49 +0200

    Changed socket url

--- src/app/config.ts
+++ src/app/config.ts
@@ -9,6 +9,6 @@ export const CONFIG = {
   },
   'production': {
     'API_URL': 'https://gmunumel-chat-rails-api-server.herokuapp.com',
-    'SOCKET_URL': process.env.REDIS_URL
+    'SOCKET_URL': 'https://gmunumel-chat-socket-io-server.herokuapp.com'
   }
 };
