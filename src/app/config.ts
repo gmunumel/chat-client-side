0bd703d9087b323011e16176a10a2bceeb4d81e8
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-06-10 11:34:55 +0200

    Added redis_url to config file

--- src/app/config.ts
+++ src/app/config.ts
@@ -9,6 +9,6 @@ export const CONFIG = {
   },
   'production': {
     'API_URL': 'https://gmunumel-chat-rails-api-server.herokuapp.com',
-    'SOCKET_URL': 'redis://h:p88d7fffee87cdb2e2dd69e1dea2d7a7c34f0b39d4cca4c7bb064d11dad4034e0@ec2-34-252-123-40.eu-west-1.compute.amazonaws.com:21829'
+    'SOCKET_URL': process.env.REDIS_URL
   }
 };
